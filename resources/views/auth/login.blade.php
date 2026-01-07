4e93c905dff18478e2461a8bf37de624f6f95bd8
Aiman Hakim <hakimbaikcr7@gmail.com>
2026-01-07 11:49:26 +0800

    feat: login form with storage link and password toggle
    - Implement password visibility toggle in login.blade.php using Alpine.js for improved UX

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,28 +13,64 @@
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
+        <div class="mt-4" x-data="{ showPassword: false }">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <!-- spacing goes HERE -->
+            <div class="relative mt-2">
+                <input
+                    id="password"
+                    x-bind:type="showPassword ? 'text' : 'password'"
+                    name="password"
+                    required
+                    autocomplete="current-password"
+                    class="block w-full pr-12 border-gray-300 dark:border-gray-700
+                    dark:bg-gray-900 dark:text-gray-300
+                    focus:border-indigo-500 dark:focus:border-indigo-600
+                    focus:ring-indigo-500 dark:focus:ring-indigo-600
+                    rounded-md shadow-sm h-10" />
+
+                <button class="block mt-1"
+                    type="button"
+                    @click="showPassword = !showPassword"
+                    class="absolute inset-y-0 right-4 flex items-center text-white dark:text-white-100 hover:text-white-300 focus:outline-none">
+                    <svg x-show="!showPassword" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M2.458 12C3.732 7.943 7.523 5 12 5
+                        c4.478 0 8.268 2.943 9.542 7
+                        -1.274 4.057-5.064 7-9.542 7
+                        -4.477 0-8.268-2.943-9.542-7z" />
+                    </svg>
+
+                    <svg x-show="showPassword" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M13.875 18.825A10.05 10.05 0 0112 19
+                       c-4.478 0-8.268-2.943-9.543-7
+                       a9.97 9.97 0 011.563-3.029
+                       m5.858.908a3 3 0 114.243 4.243
+                       M9.878 9.878l4.242 4.242
+                       M9.878 9.878L3 3m6.878 6.878L21 21" />
+                    </svg>
+                </button>
+            </div>
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
+
         <!-- Remember Me -->
-        <div class="block mt-4">
+        <div class="block mt-1">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
                 <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
             </label>
         </div>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+        <x-primary-button class="ms-1">
+            {{ __('Log in') }}
+        </x-primary-button>
         </div>
     </form>
 </x-guest-layout>
